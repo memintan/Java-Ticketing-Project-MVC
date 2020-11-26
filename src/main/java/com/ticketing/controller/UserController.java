fab552ce11eaca5bb887faadf74909f20e0d2a05
memintan <memintan@gmail.com>
2020-11-25 21:43:30 -0600

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/UserController.java
+++ src/main/java/com/ticketing/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.ticketing.controller;
 
+import com.ticketing.dto.RoleDTO;
+import com.ticketing.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping({"/create","/add", "/initialize"})
-    public String createUser(){
+    @GetMapping("/create")
+    public String createUser(Model model){
+        model.addAttribute("user",new UserDTO());
+
+        //dataGenerator
+        //
+
         return "/user/create";
     }
 }
