18b85b560b96fc425bd3cb95c4d39b57d4b812ba
memintan <memintan@gmail.com>
2021-02-05 16:08:49 -0600

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/ProjectController.java
+++ src/main/java/com/ticketing/controller/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,9 +25,13 @@ public class ProjectController {
         model.addAttribute("projects", projectService.findAll());
         model.addAttribute("managers", userService.findAll());
 
-
-
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+        projectService.save(project);
+        return"redirect:/project/create";
     }
 
 
