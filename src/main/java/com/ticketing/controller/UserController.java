cb25dec104cb77d14d54007f0dbc4097fdae8591
memintan <memintan@gmail.com>
2021-02-05 11:57:24 -0600

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/UserController.java
+++ src/main/java/com/ticketing/controller/UserController.java
@@ -2,6 +2,9 @@ package com.ticketing.controller;
 
 import com.ticketing.dto.RoleDTO;
 import com.ticketing.dto.UserDTO;
+import com.ticketing.service.RoleService;
+import com.ticketing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
+
     @GetMapping("/create")
     public String createUser(Model model){
-        model.addAttribute("user",new UserDTO());
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
         //dataGenerator
         //
