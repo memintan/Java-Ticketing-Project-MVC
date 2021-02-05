b14772d47b8a9eadb6c2dc79dff01a6b394889f6
memintan <memintan@gmail.com>
2021-02-05 16:22:33 -0600

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/ProjectController.java
+++ src/main/java/com/ticketing/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.ticketing.controller;
 
 import com.ticketing.dto.ProjectDTO;
+import com.ticketing.enums.Status;
 import com.ticketing.service.ProjectService;
 import com.ticketing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,7 @@ public class ProjectController {
     @PostMapping("/create")
     public String insertProject(ProjectDTO project){
         projectService.save(project);
+        project.setProjectStatus(Status.OPEN);
         return"redirect:/project/create";
     }
 
