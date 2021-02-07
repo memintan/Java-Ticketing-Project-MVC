b6b0913312ffa0ddfb936fd4674c1281d344f8bc
memintan <memintan@gmail.com>
2021-02-06 22:51:27 -0600

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/TaskController.java
+++ src/main/java/com/ticketing/controller/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/task")
@@ -43,6 +44,9 @@ public class TaskController {
 
         task.setTaskStatus(Status.OPEN);
         task.setAssignedDate(LocalDate.now());
+        task.setId(UUID.randomUUID().getMostSignificantBits());
+
+        System.out.println("Auto generated ID => " + task.getId());
         taskService.save(task);
 
 
--- src/main/java/com/ticketing/dto/TaskDTO.java
+++ src/main/java/com/ticketing/dto/TaskDTO.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
+import java.util.UUID;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -20,4 +21,14 @@ public class TaskDTO {
     private String taskDetail;
     private Status taskStatus;
     private LocalDate assignedDate;
+
+    public TaskDTO(ProjectDTO project, UserDTO assignedEmployee, String taskSubject, String taskDetail, Status taskStatus, LocalDate assignedDate) {
+        this.project = project;
+        this.assignedEmployee = assignedEmployee;
+        this.taskSubject = taskSubject;
+        this.taskDetail = taskDetail;
+        this.taskStatus = taskStatus;
+        this.assignedDate = assignedDate;
+        this.id = UUID.randomUUID().getMostSignificantBits();
+    }
 }
